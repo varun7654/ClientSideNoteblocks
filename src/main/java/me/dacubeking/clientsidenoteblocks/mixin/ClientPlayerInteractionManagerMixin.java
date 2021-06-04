e6cb7959493983d1536579f02b964f85db83d879
varun7654 <61074688+DaCubeKing@users.noreply.github.com>
2021-06-04 07:53:12 -0700

    init

---
+++ src/main/java/me/dacubeking/clientsidenoteblocks/mixin/ClientPlayerInteractionManagerMixin.java
@@ -0,0 +1,39 @@
+package me.dacubeking.clientsidenoteblocks.mixin;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.client.sound.SoundInstance;
+import net.minecraft.client.sound.SoundManager;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ClientPlayerInteractionManager.class)
+public abstract class ClientPlayerInteractionManagerMixin {
+
+    @Final
+    @Shadow
+    private MinecraftClient client;
+
+    @Shadow @Final private ClientPlayNetworkHandler networkHandler;
+
+    @Redirect(method = "updateBlockBreakingProgress", at = @At(value = "INVOKE" , target = "Lnet/minecraft/client/sound/SoundManager;play(Lnet/minecraft/client/sound/SoundInstance;)V"))
+    public void cancelBlockBreakSound(SoundManager soundManager, SoundInstance sound, BlockPos pos, Direction direction){
+        if(this.client.world == null){
+            soundManager.play(sound);
+            return;
+        }
+
+        if(!(this.client.world.getBlockState(pos).getBlock() == Blocks.NOTE_BLOCK)){
+            soundManager.play(sound);
+            return;
+        }
+
+    }
+}
---
+++ src/main/java/me/dacubeking/clientsidenoteblocks/mixininterfaces/SoundHandlerInterface.java
@@ -0,0 +1,7 @@
+package me.dacubeking.clientsidenoteblocks.mixininterfaces;
+
+import net.minecraft.client.sound.SoundInstance;
+
+public interface SoundHandlerInterface {
+    void bypassedPlay(SoundInstance sound);
+}
