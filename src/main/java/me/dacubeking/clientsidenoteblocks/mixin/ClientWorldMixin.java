bb434957dcc0b3b97f9a91b6e6c7a6aab510ccd6
varun7654 <varun7654@outlook.com>
2022-06-01 19:34:11 -0700

    update to 1.19

--- src/main/java/me/dacubeking/clientsidenoteblocks/mixin/ClientWorldMixin.java
+++ src/main/java/me/dacubeking/clientsidenoteblocks/mixin/ClientWorldMixin.java
@@ -9,24 +9,37 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.util.profiler.Profiler;
+import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.MutableWorldProperties;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 import java.util.Objects;
+import java.util.function.Supplier;
 
 @Mixin(ClientWorld.class)
-public class ClientWorldMixin implements ClientWorldInterface {
+public abstract class ClientWorldMixin extends World implements ClientWorldInterface{
 
     @Final
     @Shadow
     private MinecraftClient client;
 
+    //Ignored by mixin
+    protected ClientWorldMixin(MutableWorldProperties properties, RegistryKey<World> registryRef, RegistryEntry<DimensionType> dimension, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long seed, int maxChainedNeighborUpdates) {
+        super(properties, registryRef, dimension, profiler, isClient, debugWorld, seed, maxChainedNeighborUpdates);
+    }
+
     @Override
     public void bypassedPlaySound(@Nullable PlayerEntity player, BlockPos pos, SoundEvent sound, SoundCategory category, float volume, float pitch) {
         if (Objects.equals(player, this.client.player)) {
-            PositionedSoundInstance positionedSoundInstance = new PositionedSoundInstance(sound, category, volume, pitch, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5);
+            PositionedSoundInstance positionedSoundInstance = new PositionedSoundInstance(sound, category, volume, pitch, Random.create(this.random.nextLong()), pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5);
             SoundHandlerInterface soundManager = (SoundHandlerInterface) this.client.getSoundManager();
             soundManager.bypassedPlay(positionedSoundInstance);
         }
--- src/main/java/me/dacubeking/clientsidenoteblocks/mixin/SoundHandlerMixin.java
+++ src/main/java/me/dacubeking/clientsidenoteblocks/mixin/SoundHandlerMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.client.sound.SoundInstance;
 import net.minecraft.client.sound.SoundManager;
 import net.minecraft.client.sound.SoundSystem;
 import net.minecraft.util.math.BlockPos;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -17,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class SoundHandlerMixin implements SoundHandlerInterface {
 
     @Inject(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At("HEAD"), cancellable = true)
-    public void onPlay(SoundInstance sound, CallbackInfo ci) {
+    public void onPlay(@NotNull SoundInstance sound, CallbackInfo ci) {
         BlockPos pos = new BlockPos(sound.getX() - 0.5, sound.getY() - 0.5, sound.getZ() - 0.5);
 
         if (NoteblockData.cancelableNoteblockSounds.containsKey(pos)) {
